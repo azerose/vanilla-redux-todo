b92feebe94d191ea66aaadcb58822d3f9393fc89
azerose <woojjang8671@naver.com>
2022-12-02 15:41:21 +0900

    DeleteBtn has eachother id

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const reducer = (state = [], action) => {
     case ADD_TODO:
       return [...state, { text: action.text, id: Date.now() }];
     case DELETE_TODO:
-      return [];
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
@@ -35,7 +35,7 @@ const reducer = (state = [], action) => {
 const store = createStore(reducer);
 
 const dispatchDeleteToDo = (e) => {
-  const id = e.target.parentNode.id;
+  const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteToDo(id));
 };
 
